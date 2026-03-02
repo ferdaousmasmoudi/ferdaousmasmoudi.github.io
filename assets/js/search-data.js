e87fe670d759f7809c866381af1109f893f7baa3
ferdaousmasmoudi <34039665+ferdaousmasmoudi@users.noreply.github.com>
2026-03-02 07:47:40 +0000

    Deploying to gh-pages from @ ferdaousmasmoudi/ferdaousmasmoudi.github.io@d4988d26e57882cac23d51ac3f3d63d3a82ce4f1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-research-projects",
+          title: "Research Projects",
+          description: "Research and development projects",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
