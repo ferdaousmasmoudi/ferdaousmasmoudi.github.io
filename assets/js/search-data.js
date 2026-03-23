c602bf2ba0acd101ac4527792df058c586165391
ferdaousmasmoudi <34039665+ferdaousmasmoudi@users.noreply.github.com>
2026-03-23 17:05:59 +0000

    Deploying to gh-pages from @ ferdaousmasmoudi/ferdaousmasmoudi.github.io@67f5df5d428c45e9c77fcd5d6a9d44f85a7d29f7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-research-projects",
-          title: "Research Projects",
+        },{id: "nav-projects",
+          title: "Projects",
           description: "Research and development projects",
           section: "Navigation",
           handler: () => {
@@ -37,7 +37,28 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "books-the-godfather",
+        },{id: "nav-people",
+          title: "people",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
+          },
+        },{id: "dropdown-bookshelf",
+              title: "bookshelf",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/books/";
+              },
+            },{id: "dropdown-blog",
+              title: "blog",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/blog/";
+              },
+            },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
