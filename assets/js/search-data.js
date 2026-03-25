7eb1eeaab58a2f49ac3a5e38c6ade4c69adc9a93
ferdaousmasmoudi <34039665+ferdaousmasmoudi@users.noreply.github.com>
2026-03-25 09:09:21 +0000

    Deploying to gh-pages from @ ferdaousmasmoudi/ferdaousmasmoudi.github.io@eb9e5182085ee5d0dc6f285164afe1395b9cc5a1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Courses, labs, and pedagogical approach in engineering and applied sciences.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
